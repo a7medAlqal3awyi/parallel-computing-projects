f04946c7a4306d07c350b77af9f1dea020516b17
Ahmed <a7medalqalawyi@bmail.com>
2023-10-30 00:20:00 +0200

    firtst commit with sequentioal programming example

---
+++ src/sequential_example/sequentialExample.java
@@ -0,0 +1,19 @@
+package sequential_example;
+
+class sequentialExample {
+
+
+    public static void main(String[] args) {
+
+        preformTask("task 1 ");
+        preformTask("task 2 ");
+    }
+
+    public  static void preformTask(String task){
+        System.out.println("Execute "+task);
+        System.out.println(task +"Completed ");
+        System.out.println( "\n=================\n ");
+
+    }
+
+    }
