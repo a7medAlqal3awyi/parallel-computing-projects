f201c4c8e22ead218f85957c8874b288b6d92973
Ahmed <a7medalqalawyi@bmail.com>
2023-10-30 01:26:31 +0200

    finish thread concept  extending thread class

--- src/thread_class/main.java
+++ src/thread_class/main.java
@@ -5,7 +5,7 @@ import thread_class.MyThread;
 public class main {
     public static void main(String[] args) {
         MyThread myThread1 = new MyThread();
-        myThread1.start();
+         myThread1.start();
         MyThread myThread2 = new MyThread();
         myThread2.start();
     }
